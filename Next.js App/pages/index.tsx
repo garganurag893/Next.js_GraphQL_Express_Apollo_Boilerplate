13aacced2446f3cd54c121125d316a37517b18ef
anuraggarg <anurag@geekyants.com>
2020-01-08 18:11:49 +0530

    Adding ui

--- Next.js App/pages/index.tsx
+++ Next.js App/pages/index.tsx
@@ -1,34 +1,64 @@
 import React from "react";
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
+import Card from "../src/components/Card";
 
-interface User {
-  id: string;
-  name: string;
-  email: string;
-}
-
-const GET_USERS = gql`
-  {
-    users {
-      id
-      name
-      email
-    }
-  }
-`;
-
-const Home = () => {
-  const { loading, error, data } = useQuery(GET_USERS);
-
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
-
+const Home: React.SFC = () => {
   return (
-    <div>
-      {data.users.map((user: User) => (
-        <h1 key={user.id}>{user.name}</h1>
-      ))}
+    <div className="container">
+      <h1 className="heading">Welcome</h1>
+      <div className="cardContainer">
+        <Card title="Query" href="/users" />
+        <Card title="Mutation" href="/signup" />
+        <Card title="Subscription" href="/subscription" />
+      </div>
+      <style jsx>
+        {`
+          .container {
+            height: 100%;
+            min-height: 100vh;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-flow: column wrap;
+          }
+          .cardContainer {
+            display: flex;
+            flex-flow: wrap row;
+            justify-content: center;
+            align-items: center;
+          }
+          .heading {
+            color: white;
+            text-align: center;
+            font-size: 5rem;
+            padding: 0rem 0 5rem;
+          }
+        `}
+      </style>
+      <style jsx global>
+        {`
+          h1,
+          h2 {
+            margin: 0;
+            font-family: Candara;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            min-height: 100vh;
+            font-family: Candara;
+            background: #355c7d;
+            background: -webkit-linear-gradient(
+              to right,
+              #c06c84,
+              #6c5b7b,
+              #355c7d
+            );
+            background: linear-gradient(to right, #c06c84, #6c5b7b, #355c7d);
+          }
+        `}
+      </style>
     </div>
   );
 };
